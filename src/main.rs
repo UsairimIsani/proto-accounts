52215777f74159144ce334133f2a5aaf443bb1d1
Usairim Isani <usairim.isani@gmail.com>
2019-08-30 02:53:00 +0500

    Working with Hashmaps.

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,46 @@
-fn st(s: &str) -> String {
-    String::from(s)
-}
+use std::collections::HashMap;
+use std::ops::Index;
+
 fn main() {
-    unsafe {
-        let mut vec: Vec<i32> = vec![2, 3, -65, 90];
-        let size = vec.capacity() as isize - 1;
-        let ptr_to_vec = vec.as_mut_ptr();
-        let end_addr = ptr_to_vec.offset(size);
-        println!(
-            "Vector : {:?}, \nSize : {},\nPointer : {:?}\nEnd of Address : {:?}",
-            vec, size, ptr_to_vec, &end_addr
-        );
-        for i in 0..=size {
-            let j = i.clone() as isize;
-            println!(
-                "Got the Pointer {:?} and its value {:?}",
-                ptr_to_vec.offset(j),
-                *ptr_to_vec.offset(j)
-            );
-        }
-        // Vector : [2, 3, -65, 90],
-        // Size : 4,
-        // Pointer : 0x55e20f377b40
-        // End of Address : 0x55e20f377b50
-        // Got the Pointer 0x55e20f377b40 and its value 2
-        // Got the Pointer 0x55e20f377b44 and its value 3
-        // Got the Pointer 0x55e20f377b48 and its value -65
-        // Got the Pointer 0x55e20f377b4c and its value 90
-        // Got the Pointer 0x55a8f6342b50 and its value 0
-    }
-    // while_let();
-}
-fn while_let() {
-    let mut a: Vec<u8> = vec![123, 123, 123, 123, 123];
-    while let Some(i) = a.pop() {
-        print!("{}  ", i);
-        println!("{:?}", &a);
-    }
+    // let mut hashmap: HashMap<&str, i32> = HashMap::new();
+    // hashmap.insert("something", 52);
+    // let a = hashmap.keys();
+    // println!("{:?}", a);
+    // let b = "something";
+    // let c = hashmap[&b];
+    // let a = hashmap.values();
+    // println!("{:?}", a);
+    //
+    //
+    // let b = "something";
+    // let c = hashmap[&b];
+    // println!("{}", c);
+    // hashmap.insert("tomatto", 90);
+    // hashmap.entry("Mutton").or_insert(45);
+    // hashmap.entry("zama").or_default();
+    // hashmap.entry("tomatto").or_default();
+    // println!("{:?}", hashmap);
+    //
+    //
+    // let mut hashmap: HashMap<&str, Option<f64>> = HashMap::new();
+    // hashmap.entry("No ").or_default();
+    // println!("{:?}", hashmap);
+    //
+    //
+    // let mut hashmap: HashMap<&str, Option<f64>> = HashMap::new();
+    // hashmap.entry("No ").or_insert(Some(89.0000003));
+    // let a_val_of_hsmp: &Option<f64> = hashmap.index("No ");
+    // println!("{:?}", hashmap);
+    // if let Some(i) = a_val_of_hsmp {
+    //     println!("{:?}", i)
+    // }
+    //
+    //
+    // let vec_of_some: Vec<Option<i16>> = vec![Some(3), Some(2), None, Some(89), None];
+    // let vec_of_str: Vec<&str> = vec!["hello", "world", "chicken", "kebab", "paratha"];
+    // let hashmap: HashMap<_, _> = vec_of_str.iter().zip(vec_of_some.iter()).collect();
+    // println!("{:?}", hashmap);
+    // for (key, val) in hashmap.iter() {
+    //     println!("{} {:?}", key, val);
+    // }
 }
