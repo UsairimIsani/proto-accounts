ee23859255eb90353ab2ec410c38ecfd71732d99
Usairim Isani <usairim.isani@gmail.com>
2019-08-30 00:06:21 +0500

    Playing with unsafe rust and Raw Pointers.

--- src/main.rs
+++ src/main.rs
@@ -1,88 +1,40 @@
-// Enums
-// #[derive(Debug)]
-// enum IpAddr {
-//     V4(u8, u8, u8, u8),
-//     V6(String),
-// }
-// impl IpAddr {
-//     fn constructor(t: (u8, u8, u8, u8)) -> IpAddr {
-//         IpAddr::V4(t.0, t.1, t.2, t.3)
-//     }
-// }
-// fn main() {
-//     // let home = IpAddr::V4(127, 0, 0, 1);
-//     let home = IpAddr::constructor((34, 44, 4, 4));
-//     println!("{:?}", &home);
-
-//     let loopback = IpAddr::V6(String::from("::1"));
-// }
-// use std::fmt;
-// #[derive(Debug)]
-// enum Family {
-//     Father {
-//         name: String,
-//         age: u8,
-//         occupation: String,
-//     },
-// }
-// impl Family {
-//     fn constructor(father: (String, u8, String)) -> Family {
-//         Family::Father {
-//             name: father.0,
-//             age: father.1,
-//             occupation: father.2,
-//         }
-//     }
-// }
-// fn main() {
-//     let father = Family::constructor((String::from("Altaf"), 55, String::from("Doctor")));
-//     println!("{:?}", &father);
-// }
-// fn main() {
-//     let a = Some("Stringy thing");
-//     let b = Some(45);
-//     let c: Option<i32> = None;
-//     println!("{:?} {:?} {:?}", a, b, c);
-//     let d: u8 = match b {
-//         Some(i) => i as u8,
-//         None => 0,
-//     };
-//     let e: u8 = match a {
-//         Some(i) => i.len() as u8,
-//         None => 0,
-//     };
-//     println!("{}", d);
-//     println!("{}", e);
-// }
-#[derive(Debug)]
-struct Person {
-    name: String,
-    age: u8,
-}
-impl Person {
-    fn cons(name: String, age: u8) -> Option<Person> {
-        Some(Person { name, age })
-    }
+fn st(s: &str) -> String {
+    String::from(s)
 }
 fn main() {
-    let p = Person::cons(String::from("Usairim Isani"), 21);
-    // let p: Option<Person> = None;
-    println!("{:?} ", &p);
-    let mut p = match p {
-        Some(i) => i,
-        _ => panic!("all are Chickens, all Person fled"),
-    };
-    // if let Some(Person) = p {
-    //     p
-    // };
-    let b = Person::cons(String::from("name: String"), 32);
-    // let mut c;
-    if let Some(i) = &b {
-        println!("{:?}", &b);
-        println!("{:?}", &i);
-        // c = b.unwrap();
-    };
-    println!("{:?}", &b);
-    println!("{:?}", &p);
-    // println!("{:?}", &c);
+    unsafe {
+        let mut vec: Vec<i32> = vec![2, 3, -65, 90];
+        let size = vec.capacity() as isize;
+        let ptr_to_vec = vec.as_mut_ptr();
+        let end_addr = ptr_to_vec.offset(size);
+        println!(
+            "Vector : {:?}, \nSize : {},\nPointer : {:?}\nEnd of Address : {:?}",
+            vec, size, ptr_to_vec, &end_addr
+        );
+        for i in 0..=size {
+            let j = i.clone() as isize;
+            println!(
+                "Got the Pointer {:?} and its value {:?}",
+                ptr_to_vec.offset(j),
+                *ptr_to_vec.offset(j)
+            );
+        }
+        // Vector : [2, 3, -65, 90],
+        // Size : 4,
+        // Pointer : 0x55e20f377b40
+        // End of Address : 0x55e20f377b50
+        // Got the Pointer 0x55e20f377b40 and its value 2
+        // Got the Pointer 0x55e20f377b44 and its value 3
+        // Got the Pointer 0x55e20f377b48 and its value -65
+        // Got the Pointer 0x55e20f377b4c and its value 90
+        // Got the Pointer 0x55a8f6342b50 and its value 0
+    }
+    // while_let();
+}
+fn while_let() {
+    let mut a: Vec<u8> = vec![123, 123, 123, 123, 123];
+    while let Some(i) = a.pop() {
+        print!("{}  ", i);
+        println!("{:?}", &a);
+    }
 }
