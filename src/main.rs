96f83a8fe5761f956d122980befab5aa6c7a220a
Usairim Isani <usairim.isani@gmail.com>
2019-08-29 19:47:38 +0500

    Learned to Use Enums, the Enum Option<T>, the if let.

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,88 @@
-use std::fmt;
+// Enums
+// #[derive(Debug)]
+// enum IpAddr {
+//     V4(u8, u8, u8, u8),
+//     V6(String),
+// }
+// impl IpAddr {
+//     fn constructor(t: (u8, u8, u8, u8)) -> IpAddr {
+//         IpAddr::V4(t.0, t.1, t.2, t.3)
+//     }
+// }
+// fn main() {
+//     // let home = IpAddr::V4(127, 0, 0, 1);
+//     let home = IpAddr::constructor((34, 44, 4, 4));
+//     println!("{:?}", &home);
+
+//     let loopback = IpAddr::V6(String::from("::1"));
+// }
+// use std::fmt;
+// #[derive(Debug)]
+// enum Family {
+//     Father {
+//         name: String,
+//         age: u8,
+//         occupation: String,
+//     },
+// }
+// impl Family {
+//     fn constructor(father: (String, u8, String)) -> Family {
+//         Family::Father {
+//             name: father.0,
+//             age: father.1,
+//             occupation: father.2,
+//         }
+//     }
+// }
+// fn main() {
+//     let father = Family::constructor((String::from("Altaf"), 55, String::from("Doctor")));
+//     println!("{:?}", &father);
+// }
+// fn main() {
+//     let a = Some("Stringy thing");
+//     let b = Some(45);
+//     let c: Option<i32> = None;
+//     println!("{:?} {:?} {:?}", a, b, c);
+//     let d: u8 = match b {
+//         Some(i) => i as u8,
+//         None => 0,
+//     };
+//     let e: u8 = match a {
+//         Some(i) => i.len() as u8,
+//         None => 0,
+//     };
+//     println!("{}", d);
+//     println!("{}", e);
+// }
 #[derive(Debug)]
-enum Family {
-    Father {
-        name: String,
-        height: u8,
-        age: u8,
-        occupation: String,
-    },
-    // Mother {
-    //     name: String,
-    //     height: u8,
-    //     age: u8,
-    //     occupation: String,
-    // },
-    // Son {
-    //     name: String,
-    //     height: u8,
-    //     age: u8,
-    // },
-    // Daughter {
-    //     name: String,
-    //     height: u8,
-    //     age: u8,
-    // },
-}
-#[derive(Debug)]
-struct Father {
+struct Person {
     name: String,
-    height: u8,
     age: u8,
-    occupation: String,
 }
-impl Father {
-    fn constructor(name: String, height: u8, age: u8, occupation: String) -> Father {
-        Father {
-            name,
-            height,
-            age,
-            occupation,
-        }
-    }
-    fn introduce(&self) {
-        println!("{} ", &self)
-    }
-}
-impl fmt::Display for Father {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        writeln!(
-            f,
-            "Hi, I am {}, I am  {} old, I am {} tall and I am a {}",
-            &self.name, &self.age, &self.height, &self.occupation
-        )
+impl Person {
+    fn cons(name: String, age: u8) -> Option<Person> {
+        Some(Person { name, age })
     }
 }
 fn main() {
-    let father = Father::constructor(
-        String::from("Altaf Hussain"),
-        128,
-        55,
-        String::from("Doctor"),
-    );
-    father.introduce();
+    let p = Person::cons(String::from("Usairim Isani"), 21);
+    // let p: Option<Person> = None;
+    println!("{:?} ", &p);
+    let mut p = match p {
+        Some(i) => i,
+        _ => panic!("all are Chickens, all Person fled"),
+    };
+    // if let Some(Person) = p {
+    //     p
+    // };
+    let b = Person::cons(String::from("name: String"), 32);
+    // let mut c;
+    if let Some(i) = &b {
+        println!("{:?}", &b);
+        println!("{:?}", &i);
+        // c = b.unwrap();
+    };
+    println!("{:?}", &b);
+    println!("{:?}", &p);
+    // println!("{:?}", &c);
 }
