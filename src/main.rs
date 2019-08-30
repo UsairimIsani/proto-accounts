924369376233be4cfa7b3dfea1f43a6c81f2e408
Usairim Isani <usairim.isani@gmail.com>
2019-08-31 03:21:28 +0500

    hashmaps and Deref(still unclear)

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,13 @@
 use std::collections::HashMap;
-use std::ops::Index;
-
 fn main() {
-    // let mut hashmap: HashMap<&str, i32> = HashMap::new();
-    // hashmap.insert("something", 52);
-    // let a = hashmap.keys();
-    // println!("{:?}", a);
-    // let b = "something";
-    // let c = hashmap[&b];
-    // let a = hashmap.values();
-    // println!("{:?}", a);
-    //
-    //
-    // let b = "something";
-    // let c = hashmap[&b];
-    // println!("{}", c);
-    // hashmap.insert("tomatto", 90);
-    // hashmap.entry("Mutton").or_insert(45);
-    // hashmap.entry("zama").or_default();
-    // hashmap.entry("tomatto").or_default();
-    // println!("{:?}", hashmap);
-    //
-    //
-    // let mut hashmap: HashMap<&str, Option<f64>> = HashMap::new();
-    // hashmap.entry("No ").or_default();
-    // println!("{:?}", hashmap);
-    //
-    //
-    // let mut hashmap: HashMap<&str, Option<f64>> = HashMap::new();
-    // hashmap.entry("No ").or_insert(Some(89.0000003));
-    // let a_val_of_hsmp: &Option<f64> = hashmap.index("No ");
-    // println!("{:?}", hashmap);
-    // if let Some(i) = a_val_of_hsmp {
-    //     println!("{:?}", i)
-    // }
-    //
-    //
-    // let vec_of_some: Vec<Option<i16>> = vec![Some(3), Some(2), None, Some(89), None];
-    // let vec_of_str: Vec<&str> = vec!["hello", "world", "chicken", "kebab", "paratha"];
-    // let hashmap: HashMap<_, _> = vec_of_str.iter().zip(vec_of_some.iter()).collect();
-    // println!("{:?}", hashmap);
-    // for (key, val) in hashmap.iter() {
-    //     println!("{} {:?}", key, val);
-    // }
+    life_time_playground()
+}
+fn life_time_playground() {
+    let text = "hello world world world wonderful world";
+    let mut map = HashMap::new();
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+        println!("{} ", count);
+    }
 }
