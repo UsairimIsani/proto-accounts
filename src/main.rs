e9e3c99e3dba6c5f46bdd1b93459119ed9ff9d6b
Usairim Isani <usairim.isani@gmail.com>
2019-09-03 23:05:32 +0500

    Implemented my own Option Enum.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
+use std::fmt::Debug;
+enum Shirt<T>
+where
+    T: Debug + PartialEq,
+{
+    Co(T),
+    Cd,
+}
+use crate::Shirt::{Cd, Co};
 fn main() {
-    let a = 55;
-    let b = &a;
-    let c = &b;
-    assert_ne!(55, **c);
+    let chic = Co(Box::new([1, 2, 4, 5, 6]));
+    let some: Result<_, _> = match chic {
+        Co(s) => Ok(s),
+        Cd => Err(0),
+    };
+    println!("{:?}", some.unwrap());
 }
