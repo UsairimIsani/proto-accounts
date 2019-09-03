fe7b1b228925e54fdbc8b9f94b892878fe2949f4
Usairim Isani <usairim.isani@gmail.com>
2019-09-03 12:37:00 +0500

    Docs, Generics , Lifetimes

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,17 @@ fn main() {
     let v = String::from("Chicken");
     let res = longest_with_an_announcement("chic", "mut", &v);
     println!("{}", res);
+    let a: &'static str = "Chickem";
 }
 use std::fmt::{Debug, Display};
-fn longest_with_an_announcement<'a, 'b, 'c, T>(x: &'a str, y: &'b str, ann: &'c T) -> &'c T
+/// Start Here
+/// # Example
+/// ``` Chicken Chicken
+///
+///
+/// ```
+/// ### Mutton
+pubfn longest_with_an_announcement<'a, 'b, 'c, T>(x: &'a str, y: &'b str, ann: &'c T) -> &'c T
 where
     T: Debug,
 {
