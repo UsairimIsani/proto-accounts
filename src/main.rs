6323adc0470c35ef75fb17278492208c338b30ab
Usairim Isani <usairim.isani@gmail.com>
2019-09-03 19:18:19 +0500

    Basic References

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,6 @@
-#[derive(PartialEq, Debug)]
-struct Shirt {
-    size: u32,
-    brand: String,
-    quality: u32,
-    quantity: u32,
-}
-impl Shirt {
-    fn new(size: u32, brand: String, quality: u32, quantity: u32) -> Shirt {
-        Shirt {
-            size,
-            brand,
-            quality,
-            quantity,
-        }
-    }
-}
-impl Iterator for Shirt {
-    type Item = u32;
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.quantity >= 0 {
-            self.quantity -= 1;
-            Some(self.quantity)
-        } else {
-            println!("{}", self.quantity);
-            None
-        }
-    }
-}
 fn main() {
-    let mut a: Shirt = Shirt::new(15, String::from("Nike"), 50, 70);
-    println!("{:?}", a);
-    let b = a.quantity;
-    let b = b as i32;
-    for i in 0..b {
-        let some = &a.next();
-        match some {
-            Some(t) => println!("Shirts Here {:?}", t),
-            None => println!("All Out"),
-        }
-    }
-    println!("{:?}", a);
+    let a = 55;
+    let b = &a;
+    let c = &b;
+    assert_ne!(55, **c);
 }
